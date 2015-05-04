763810d475ef29d4e616e293ca0ed26f8afe1a9e
Rajiv Fonn <rfonn@te-rajiv.core.setec.com.au>
2015-05-04 12:30:30 +1000

    Added comment.

--- smu75070_final/limit.py
+++ smu75070_final/limit.py
@@ -3,6 +3,7 @@
 
 # Tuple ( Tuple (name, identity, low, high, string, boolean))
 DATA = (
+    # 70 +/- 0.7
     ('70VOn', 1, 69.3, 70.7, None, None),
     ('70VOff', 1, -0.5, 69.2, None, None),
     # 11.5 +/- 0.1
