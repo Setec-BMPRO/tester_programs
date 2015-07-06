5178a02dd829c12bee9b1c9cba143f3b8ba19408
Stephen Bell <stephen.bell@setec.com.au>
2015-07-06 23:01:47 +1000

    Create 1st generation ARM console driver.

---
+++ share/arm_gen1.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+"""1st Generation ARM Processor Console Driver.
+
+Communication is done with a Serial port to the ARM processor.
+
+Commands are sent, followed by a '\r' trigger character.
+    Each character of the command is echoed as it is sent.
+    The '\r' is not echoed as it is sent, as there are often other response
+    characters before and after the '\r'...
+    The LPC1549 device does NOT have a buffered UART, so a character cannot
+    be sent until the previous one has been echoed back.
+
+After a command has been sent, the string ' -> ' is (usually?) sent back,
+followed by any command response, and then followed by '\r\n> '.
+That is the line termination of CR LF, then a user prompt of '> '.
+For example:
+    A command with no return value: 'NV-DEFAULT'
+    will return the echo and response: 'NV-DEFAULT -> \r\n> '
+and:
+    A command with a return value: 'X-SOFTWARE-VERSION x?'
+    will return the echo and response: 'X-SOFTWARE-VERSION x? -> 2\r\n> '
+
+"""
+
+import logging
+import time
+
+# Command trigger
+_CMD_RUN = b'\r'
+# Command suffix (between command echo and command response)
+_CMD_SUFFIX = b' -> '
+# Command prompt (after command response)
+_CMD_PROMPT = b'\r\n> '
+
+
+class EchoError(Exception):
+
+    """Command Echo Error."""
+
+
+class ArmConsoleGen1():
+
+    """Communications to First Generation ARM console."""
+
+    def __init__(self, serport):
+        """Initialise communications.
+
+        Set an appropriate serial read timeout.
+        @param serport Opened serial port to use.
+
+        """
+        self._logger = logging.getLogger(
+            '.'.join((__name__, self.__class__.__name__)))
+        self._ser = serport
+        self._ser.timeout = 10240 / self._ser.baudrate  # Timeout of 1k bytes
+        self.flush()
+
+    def flush(self):
+        """Flush input (both serial port and buffer)."""
+        # See what is waiting
+        buf = self._ser.read(10240)
+        if len(buf) > 0:
+            # Show what we are flushing
+            self._logger.debug('flush() %s', buf)
+        self._ser.flushInput()
+
+    def action(self, command=None, delay=0):
+        """Send a command, and read the response line(s).
+
+        @param command Command string.
+        @param delay Delay between sending command and reading response.
+        @return Response (a List of received line Strings).
+
+        """
+        if command:
+            self._write_command(command)
+        if delay:
+            time.sleep(delay)
+        return self._read_response()
+
+    def _read_response(self):
+        """Read a response.
+
+        @return Response (a List of received line Strings).
+
+        """
+        # Read until a timeout happens
+        buf = self._ser.read(1024)
+        # Remove leading _CMD_SUFFIX
+        if buf.startswith(_CMD_SUFFIX):
+            buf = buf[len(_CMD_SUFFIX):]
+        # Remove trailing _CMD_PROMPT
+        if buf.endswith(_CMD_PROMPT):
+            buf = buf[:-len(_CMD_PROMPT)]
+        if len(buf) > 0:
+            response = buf.decode().splitlines()
+        else:
+            response = None
+        self._logger.debug('Read_Response: %s', response)
+        return response
+
+    def _write_command(self, command):
+        """Write a command.
+
+        The echo of each command character sent is read back.
+        The echo of _CMD_RUN is NOT expected, or read.
+        @param command Command string.
+        @raises EchoError if the command does not echo.
+
+        """
+        self._logger.debug('Write_Command: %s', repr(command))
+        send_data = command.encode()
+        # Flush input to be able to read echoed characters
+        self.flush()
+        # Send each byte with echo verification
+        for a_byte in send_data:
+            self._ser.write(a_byte)
+            echo = self._ser.read(1)
+            if echo != a_byte:
+                raise EchoError
+        # And the command RUN, without echo
+        self._ser.write(_CMD_RUN)
