03f10e07680ef4f685ddfad69da6186c3dc8954c
Stephen Bell <stephen.bell@setec.com.au>
2015-11-30 16:51:42 +1100

    Setup BC15 Final at tester.

--- programs/bc15/final/limit.py
+++ programs/bc15/final/limit.py
@@ -5,8 +5,8 @@
 #   Tuple ( Tuple (name, identity, low, high, string, boolean))
 DATA = (
     ('Notify', 1, None, None, None, True),
-    ('VoutNL', 0, 14.40 * 0.99, 14.40 * 1.01, None, None),
-    ('Vout', 0, 14.40 * 0.95, 14.40 * 1.05, None, None),
+    ('VoutNL', 0, 13.85 * 0.99, 13.85 * 1.01, None, None),
+    ('Vout', 0, 13.85 * 0.95, 13.85 * 1.05, None, None),
     ('InOCP', 0, 12.0, None, None, None),
-    ('OCP', 0, 14.0 - 1.0, 14.0 + 1.0, None, None),
+    ('OCP', 0, 14.0 - 2.0, 14.0 + 2.0, None, None),
     )
