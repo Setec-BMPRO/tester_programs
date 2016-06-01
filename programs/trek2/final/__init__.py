8f537b7e83559ddb16bc67ae7e0bd19a2ab52547
Stephen Bell <stephen.bell@setec.com.au>
2016-06-01 10:49:25 +1000

    Remove un-needed line.

--- programs/trek2/final/__init__.py
+++ programs/trek2/final/__init__.py
@@ -61,7 +61,6 @@ class Final(tester.TestSequence):
         self._logger.info('Close')
         global d, s, m
         # Switch off the USB hub & Serial ports
-        d.tunnel.close()
         d.dcs_Vcom.output(0.0, output=False)
         m = d = s = None
         super().close()
