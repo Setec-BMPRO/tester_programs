9cc9206db3b92f5c96263a459aa42502ccce5c0f
Rajiv Fonn <rajiv.fonn@setec.com.au>
2016-06-29 16:28:07 +1000

    Changed ARM port to USB COM7.

--- programs/sx750/initial/limit.py
+++ programs/sx750/initial/limit.py
@@ -14,7 +14,7 @@ PIC_HEX1 = 'sx750_pic5Vsb_1.hex'
 PIC_HEX2 = 'sx750_picPwrSw_2.hex'
 
 # Serial port for the ARM. Used by programmer and ARM comms module.
-ARM_PORT = {'posix': '/dev/ttyUSB0', 'nt': 'COM1'}[os.name]
+ARM_PORT = {'posix': '/dev/ttyUSB0', 'nt': 'COM7'}[os.name]
 # Serial port for the Arduino.
 ARDUINO_PORT = {'posix': '/dev/ttyACM0', 'nt': 'COM5'}[os.name]
 # Software image filenames
