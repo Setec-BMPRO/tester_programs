dc7335f7ebf6143ace16a3d7c6730c0d4e6e00c9
Stephen Bell <stephen.bell@setec.com.au>
2017-02-05 20:11:48 +1100

    Configure logging when testsuite.py is run stand-alone.

--- testsuite.py
+++ testsuite.py
@@ -4,6 +4,14 @@
 """Test suite definition for Eric Unittest plugin."""
 
 import unittest
+import logging
+
+# Configuration of console logger when this script is run stand-alone.
+#   Log Levels are: CRITICAL,ERROR,WARNING,INFO,DEBUG
+_LOG_LEVEL = logging.CRITICAL
+_LOG_FORMAT = '%(asctime)s:%(name)s:%(threadName)s:%(levelname)s:%(message)s'
+# Set these logger names to INFO level
+_LOGGER_NAMES = ('gpib', )
 
 
 def suite():
@@ -16,8 +24,32 @@ def suite():
         start_dir='.', pattern='test_*.py')
 
 
+def logging_setup():
+    """Setup the logging system.
+
+    Messages are sent to the stderr console.
+
+    """
+    # create console handler and set level
+    hdlr = logging.StreamHandler()
+    hdlr.setLevel(_LOG_LEVEL)
+    # Log record formatter
+    fmtr = logging.Formatter(_LOG_FORMAT)
+    # Connect it all together
+    hdlr.setFormatter(fmtr)
+    if not logging.root.hasHandlers():
+        logging.root.addHandler(hdlr)
+    logging.root.setLevel(_LOG_LEVEL)
+    # Suppress lower level logging level
+    if _LOG_LEVEL < logging.INFO:
+        for name in _LOGGER_NAMES:
+            log = logging.getLogger(name)
+            log.setLevel(logging.INFO)
+
+
 def _main():
     """Run the testsuite."""
+    logging_setup()
     runner = unittest.TextTestRunner()
     testsuite = suite()
     runner.run(testsuite)
