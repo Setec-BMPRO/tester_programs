dd2811e88c179ff4ab9f1f92ca2b3ea721be0af8
Stephen Bell <stephen.bell@setec.com.au>
2017-05-03 12:45:17 +1000

    Convert SX-750 Initial from 2nd gen to 3rd gen.

--- tests/test_sx750_initial.py
+++ tests/test_sx750_initial.py
@@ -16,52 +16,55 @@ class SX750Initial(ProgramTestCase):
 
     def test_pass_run(self):
         """PASS run of the program."""
-        sen = self.test_program.sensor
-        dev = self.test_program.logdev
-        dev.arm_ser.flushInput()        # Flush console input buffer
-        dev.ard_ser.flushInput()        # Flush Arduino input buffer
+        sen = self.test_program.sensors
+        dev = self.test_program.devices
+        dev['arm_ser'].flushInput()        # Flush console input buffer
+        dev['ard_ser'].flushInput()        # Flush Arduino input buffer
         data = {
             UnitTester.key_sen: {       # Tuples of sensor data
                 'FixtureLock':(
-                    (sen.Lock, 10.1), (sen.Part, 10.2), (sen.R601, 2001.0),
-                    (sen.R602, 2002.0), (sen.R609, 2003.0), (sen.R608, 2004.0),
+                    (sen['Lock'], 10.1), (sen['Part'], 10.2),
+                    (sen['R601'], 2001.0), (sen['R602'], 2002.0),
+                    (sen['R609'], 2003.0), (sen['R608'], 2004.0),
                     ),
                 'Program': (
-                    (sen.o5Vsb, 5.75), (sen.o5Vsbunsw, (5.0,) * 2),
-                    (sen.o3V3, 3.21), (sen.o8V5Ard, 8.5), (sen.PriCtl, 12.34),
+                    (sen['o5Vsb'], 5.75), (sen['o5Vsbunsw'], (5.0,) * 2),
+                    (sen['o3V3'], 3.21), (sen['o8V5Ard'], 8.5),
+                    (sen['PriCtl'], 12.34),
                     ),
-                'Initialise': ((sen.o5Vsb, 5.75), (sen.o5Vsbunsw, 5.01), ),
+                'Initialise': ((sen['o5Vsb'], 5.75), (sen['o5Vsbunsw'], 5.01), ),
                 'PowerUp': (
-                    (sen.ACin, 240.0), (sen.PriCtl, 12.34), (sen.o5Vsb, 5.05),
-                    (sen.o12V, (0.12, 12.34)), (sen.o24V, (0.24, 24.34)),
-                    (sen.ACFAIL, 5.0), (sen.PGOOD, 0.123),
-                    (sen.PFC,
+                    (sen['ACin'], 240.0), (sen['PriCtl'], 12.34),
+                    (sen['o5Vsb'], 5.05), (sen['o12V'], (0.12, 12.34)),
+                    (sen['o24V'], (0.24, 24.34)), (sen['ACFAIL'], 5.0),
+                    (sen['PGOOD'], 0.123),
+                    (sen['PFC'],
                         (432.0, 432.0,     # Initial reading
                          433.0, 433.0,     # After 1st cal
                          433.0, 433.0,     # 2nd reading
                          435.0, 435.0,     # Final value
                         )),
                     ),
-                '5Vsb': ((sen.o5Vsb, (5.20, 5.15, 5.14, 5.10, )), ),
+                '5Vsb': ((sen['o5Vsb'], (5.20, 5.15, 5.14, 5.10, )), ),
                 '12V': (
-                    (sen.o12V, (12.34, 12.25, 12.10, 12.00, 12.34, )),
+                    (sen['o12V'], (12.34, 12.25, 12.10, 12.00, 12.34, )),
                     # OPC SET: Push 32 reads before OCP detected
                     # OCP CHECK: Push 37 reads before OCP detected
-                    (sen.o12VinOCP,
+                    (sen['o12VinOCP'],
                         ((0.123, ) * 32 + (4.444, )) +
                         ((0.123, ) * 37 + (4.444, ))),
                     ),
                 '24V': (
-                    (sen.o24V, (24.44, 24.33, 24.22, 24.11, 24.24)),
+                    (sen['o24V'], (24.44, 24.33, 24.22, 24.11, 24.24)),
                     # OPC SET: Push 32 reads before OCP detected
                     # OCP CHECK: Push 18 reads before OCP detected
-                    (sen.o24VinOCP,
+                    (sen['o24VinOCP'],
                         ((0.123, ) * 32 + (4.444, )) +
                         ((0.123, ) * 18 + (4.444, ))),
                     ),
                 'PeakPower': (
-                    (sen.o5Vsb, 5.15), (sen.o12V, 12.22), (sen.o24V, 24.44),
-                    (sen.PGOOD, 0.15),
+                    (sen['o5Vsb'], 5.15), (sen['o12V'], 12.22),
+                    (sen['o24V'], 24.44), (sen['PGOOD'], 0.15),
                     ),
                 },
             UnitTester.key_con: {       # Tuples of console strings
@@ -82,7 +85,8 @@ class SX750Initial(ProgramTestCase):
                 },
             }
         self.tester.ut_load(
-            data, self.test_program.fifo_push, dev.arm.puts, dev.ard.puts)
+            data, self.test_program.fifo_push,
+            dev['arm'].puts, dev['ard'].puts)
         self.tester.test(('UUT1', ))
         result = self.tester.ut_result
         self.assertEqual('P', result.code)          # Test Result
