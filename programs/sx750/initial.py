ab056b9dbdbf512c677a40e5ba7ad5a95cfd3424
Rajiv Fonn <rajiv.fonn@setec.com.au>
2017-07-13 16:17:48 +1000

    Changed dcs_Vcom input to 12V.

--- programs/sx750/initial.py
+++ programs/sx750/initial.py
@@ -427,7 +427,7 @@ class LogicalDevices(share.LogicalDevices):
         self['ard'] = arduino.Arduino(ard_ser, verbose=False)
         # Switch on power to fixture circuits
         self['dcs_Arduino'].output(12.0, output=True)
-        self['dcs_Vcom'].output(9.0, output=True)
+        self['dcs_Vcom'].output(12.0, output=True)
         self['dcs_DigPot'].output(12.0, output=True)
         time.sleep(2)   # Allow OS to detect the new ports
 
