d995f4d06421056c637c4a0df4bca9091a611d48
Stephen Bell <stephen.bell@setec.com.au>
2017-10-18 10:18:36 +1100

    Add CAN bus connectivity to BP35 Final Test.

--- programs/bp35/final.py
+++ programs/bp35/final.py
@@ -3,16 +3,29 @@
 # Copyright 2017 SETEC Pty Ltd
 """BP35 Final Test Program."""
 
+import os
+import serial
 import tester
 import share
+from . import console
+from . import config
 
 
 class Final(share.TestSequence):
 
     """BP35 Final Test Program."""
 
+    arm_version = config.ARM_SW_VERSION
+    # Serial port for CAN interface
+    can_port = {'posix': '/dev/ttyUSB0', 'nt': 'COM20'}[os.name]
+    # CAN ID
+    can_id = 16
+    # Test limits
     limitdata = (
         tester.LimitDelta('Vbat', 12.8, 0.2, doc='Output voltage'),
+        tester.LimitRegExp(
+            'ARM-SwVer', '^{0}$'.format(arm_version.replace('.', r'\.')),
+            doc='Software version'),
         )
 
     def open(self):
@@ -20,14 +33,27 @@ class Final(share.TestSequence):
         super().open(self.limitdata, Devices, Sensors, Measurements)
         self.steps = (
             tester.TestStep('PowerUp', self._step_powerup),
+            tester.TestStep('CAN', self._step_can),
             )
+        self.sernum = None
 
     @share.teststep
     def _step_powerup(self, dev, mes):
         """Power-Up the Unit and measure output voltages."""
+        self.sernum = self.get_serial(self.uuts, 'SerNum', 'ui_sernum')
         dev['acsource'].output(voltage=240.0, output=True)
         self.measure(('dmm_vbat', 'ui_yesnogreen',), timeout=10)
 
+    @share.teststep
+    def _step_can(self, dev, mes):
+        """Access the unit console using the CAN bus."""
+        bp35 = dev['bp35']
+        bp35.open()
+        mes['arm_swver']()
+        # Set unit internal Serial Number to match the outside label
+        bp35['SER_ID'] = self.sernum
+        bp35['NVWRITE'] = True
+        bp35.close()
 
 class Devices(share.Devices):
 
@@ -37,10 +63,14 @@ class Devices(share.Devices):
         """Create all Instruments."""
         self['dmm'] = tester.DMM(self.physical_devices['DMM'])
         self['acsource'] = tester.ACSource(self.physical_devices['ACS'])
+        port = serial.Serial(Final.can_port, baudrate=115200, timeout=0.1)
+        tunnel = share.ConsoleCanTunnel(port, Final.can_id)
+        self['bp35'] = console.TunnelConsole(tunnel)
 
     def reset(self):
         """Reset instruments."""
         self['acsource'].reset()
+        self['bp35'].close()
 
 
 class Sensors(share.Sensors):
@@ -53,9 +83,17 @@ class Sensors(share.Sensors):
         self['vbat'] = tester.sensor.Vdc(
             self.devices['dmm'], high=2, low=2, rng=100, res=0.001)
         self['vbat'].doc = 'Unit output'
+        bp35 = self.devices['bp35']
+        self['arm_swver'] = console.Sensor(
+            bp35, 'SW_VER', rdgtype=sensor.ReadingString)
         self['yesnogreen'] = sensor.YesNo(
             message=tester.translate('bp35_final', 'IsOutputLedGreen?'),
             caption=tester.translate('bp35_final', 'capOutputLed'))
+        self['yesnogreen'].doc = 'Tester operator'
+        self['sernum'] = sensor.DataEntry(
+            message=tester.translate('bp35_final', 'msgSnEntry'),
+            caption=tester.translate('bp35_final', 'capSnEntry'))
+        self['sernum'].doc = 'Barcode scanner'
 
 
 class Measurements(share.Measurements):
@@ -67,4 +105,6 @@ class Measurements(share.Measurements):
         self.create_from_names((
             ('dmm_vbat', 'Vbat', 'vbat', 'Output ok'),
             ('ui_yesnogreen', 'Notify', 'yesnogreen', 'LED Green'),
+            ('ui_sernum', 'SerNum', 'sernum', 'Unit serial number'),
+            ('arm_swver', 'ARM-SwVer', 'arm_swver', 'Unit software version'),
             ))
--- tests/programs/test_bp35_final.py
+++ tests/programs/test_bp35_final.py
@@ -3,6 +3,7 @@
 # Copyright 2017 SETEC Pty Ltd
 """UnitTest for BP35 Final Test program."""
 
+from unittest.mock import patch
 from ..data_feed import UnitTester, ProgramTestCase
 from programs import bp35
 
@@ -14,6 +15,19 @@ class BP35Final(ProgramTestCase):
     prog_class = bp35.Final
     parameter = None
     debug = False
+    sernum = 'A1626010123'
+
+    def setUp(self):
+        """Per-Test setup."""
+        for target in (
+                'serial.Serial',
+                'share.ConsoleCanTunnel',
+                'programs.bp35.console.TunnelConsole',
+                ):
+            patcher = patch(target)
+            self.addCleanup(patcher.stop)
+            patcher.start()
+        super().setUp()
 
     def test_pass_run(self):
         """PASS run of the program."""
@@ -21,8 +35,12 @@ class BP35Final(ProgramTestCase):
         data = {
             UnitTester.key_sen: {       # Tuples of sensor data
                 'PowerUp': (
+                    (sen['sernum'], self.sernum),
                     (sen['vbat'], 12.8),
                     (sen['yesnogreen'], True),
+                    ),
+                'CAN': (
+                    (sen['arm_swver'], bp35.config.ARM_SW_VERSION),
                     ),
                 },
             }
@@ -30,5 +48,5 @@ class BP35Final(ProgramTestCase):
         self.tester.test(('UUT1', ))
         result = self.tester.ut_result
         self.assertEqual('P', result.code)
-        self.assertEqual(2, len(result.readings))
-        self.assertEqual(['PowerUp'], self.tester.ut_steps)
+        self.assertEqual(4, len(result.readings))
+        self.assertEqual(['PowerUp', 'CAN'], self.tester.ut_steps)
