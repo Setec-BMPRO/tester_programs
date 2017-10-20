5528f3525e482c137968e26ab80a555ef804fc2d
Stephen Bell <stephen.bell@setec.com.au>
2017-10-20 15:35:57 +1100

    Bump Trek2 hardware revision 5B to 6A.

--- programs/trek2/config.py
+++ programs/trek2/config.py
@@ -5,6 +5,6 @@
 # Software version
 SW_VERSION = '1.5.15833.150'
 # Hardware version (Major [1-255], Minor [1-255], Mod [character])
-HW_VERSION = (5, 0, 'B')
+HW_VERSION = (6, 0, 'A')
 # CAN Device ID
 CAN_ID = 32
