93fa17fe7f563ce94e540711c16efed40f3637b2
Rajiv Fonn <rajiv.fonn@setec.com.au>
2017-10-31 16:06:37 +1100

    Increased serial timeout.

--- programs/trsrfm/initial.py
+++ programs/trsrfm/initial.py
@@ -80,10 +80,9 @@ class Initial(share.TestSequence):
     def _step_bluetooth(self, dev, mes):
         """Test the Bluetooth interface."""
         btmac = mes['arm_btmac']().reading1
-        dev['dcs_vin'].output(0.0, True, delay=3.0)
-        dev['rla_pair_btn'].set_on(delay=0.1)
-        dev['dcs_vin'].output(self.vbatt, True, delay=5.0)
-        dev['rla_pair_btn'].set_off()
+        dev['dcs_vin'].output(0.0, True, delay=1.0)
+        dev['rla_pair_btn'].set_on(delay=0.2)
+        dev['dcs_vin'].output(self.vbatt, True)
         self._logger.debug('Scanning for Bluetooth MAC: "%s"', btmac)
         ble = dev['ble']
         ble.open()
@@ -118,7 +117,7 @@ class Devices(share.Devices):
         # Console driver
         self['trsrfm'] = console.Console(trsrfm_ser)
         # Serial connection to the BLE module
-        ble_ser = serial.Serial(baudrate=115200, timeout=0.1, rtscts=True)
+        ble_ser = serial.Serial(baudrate=115200, timeout=5.0, rtscts=True)
         # Set port separately, as we don't want it opened yet
         ble_ser.port = share.port('030451', 'BLE')
         self['ble'] = share.BleRadio(ble_ser)
