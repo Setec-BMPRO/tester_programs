73688b86733f05c767113533b6fbe1b5d3c239b0
Stephen Bell <stephen.bell@setec.com.au>
2017-11-03 11:46:14 +1100

    Allow for a different LED override command in BLE2CAN.

--- programs/ble2can/console.py
+++ programs/ble2can/console.py
@@ -6,7 +6,6 @@ import share
 
 # Some easier to use short names
 Sensor = share.Sensor
-ParameterOverride = share.ParameterOverride
 
 
 class Console(share.SamB11Console):
@@ -14,9 +13,25 @@ class Console(share.SamB11Console):
     """Communications to BLE2CAN console."""
 
     cmd_data = {
-        # Override commands
-        'RED_LED': ParameterOverride('TRS_RFM_RED_LED_OVERRIDE'),
-        'GREEN_LED': ParameterOverride('TRS_RFM_GREEN_LED_OVERRIDE'),
-        'BLUE_LED': ParameterOverride('TRS_RFM_BLUE_LED_OVERRIDE'),
+        # LED override command, a bit-mapped field:
+        #   b0 = Red, b1 = Green, b2 = Blue
+        # Values:
+        #   0 = LED off, 1 = LED on
+        # "-1" for normal LED operation
+        'LEDS': share.ParameterHex(
+            'LEDS_OVERRIDE', writeable=True, readable=False),
         }
-    override_commands = ('RED_LED', 'GREEN_LED', 'BLUE_LED')
+
+    def override(self, state=share.Override.normal):
+        """Manually override LED operation.
+
+        @param state Override enumeration
+
+        """
+        if state == share.Override.normal:
+            value = 0xFFFFFFFF
+        if state == share.Override.force_on:
+            value = 0b111
+        if state == share.Override.force_off:
+            value = 0b000
+        self['LEDS'] = value
