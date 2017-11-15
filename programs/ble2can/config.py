9d81ca23b45f9a7a28beca87a578c0c293cda6f0
Rajiv Fonn <rajiv.fonn@setec.com.au>
2017-11-15 18:21:26 +1100

    Merge branch 'master' of git.mel.setec.com.au:te/python/tester_programs

--- programs/ble2can/config.py
+++ programs/ble2can/config.py
@@ -3,6 +3,6 @@
 """BLE2CAN Configuration."""
 
 # Software binary version
-SW_VERSION = '1.1.16801.2488'
+SW_VERSION = '1.1.16808.2491'
 # Hardware version (Major [1-255], Minor [1-255], Mod [character])
 HW_VERSION = (5, 0, 'A')
