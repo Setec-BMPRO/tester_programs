1f1d3e5a9469f053d09dc7004b6c973eab86fd2f
Rajiv Fonn <rajiv.fonn@setec.com.au>
2018-03-01 09:53:24 +1100

    Revert back change.

--- programs/trs2/config.py
+++ programs/trs2/config.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 """TRS2 Configuration."""
 
-SW_VERSION = '1.5.15833.150'
+SW_VERSION = '1.0.16487.472'
 # Hardware version (Major [1-255], Minor [1-255], Mod [character])
 HW_VERSION = (4, 0, 'A')
