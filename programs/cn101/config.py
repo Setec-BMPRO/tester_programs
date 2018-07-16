259ddcf3235f5f83517cf0383eb147d415631526
Stephen Bell <stephen.bell@setec.com.au>
2018-07-16 12:39:34 +1000

    Implement ECO-15867 - CN101T version.

--- programs/cn101/config.py
+++ programs/cn101/config.py
@@ -3,6 +3,6 @@
 """CN101 Configuration."""
 
 # Software binary version
-SW_VERSION = '1.1.13665.176'
+SW_VERSION = '1.2.17835.298'
 # Hardware version
-HW_VERSION = (4, 0, 'A')
+HW_VERSION = (6, 0, 'A')
