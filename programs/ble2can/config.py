41f96abcb8bb06772fd28f45d20ee4b167292c94
Stephen Bell <stephen.bell@setec.com.au>
2018-09-06 17:17:32 +1000

    Update software version.

--- programs/ble2can/config.py
+++ programs/ble2can/config.py
@@ -3,6 +3,6 @@
 """BLE2CAN Configuration."""
 
 # Software binary version
-SW_VERSION = '1.2.16920.2623'
+SW_VERSION = '1.2.16964.2723'
 # Hardware version (Major [1-255], Minor [1-255], Mod [character])
 HW_VERSION = (5, 0, 'A')
