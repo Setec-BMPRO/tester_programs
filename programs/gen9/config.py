1773e5e42d042523d0292ae6d9c3879c79348aad
Stephen Bell <stephen.bell@setec.com.au>
2018-12-17 12:03:32 +1100

    Update GEN9 firmware.
    Fix for PFC adjustment not having effect until a restart.

--- programs/gen9/config.py
+++ programs/gen9/config.py
@@ -4,6 +4,6 @@
 
 SW_VER = '1.0'
 SW_BUILD = '2502'
-SW_IMAGE = SW_VER + '.18359.' + SW_BUILD
+SW_IMAGE = SW_VER + '.18365.' + SW_BUILD
 # Hardware version (Major [1-255], Minor [1-255], Mod [character])
 HW_VERSION = (5, 0, 'A')
