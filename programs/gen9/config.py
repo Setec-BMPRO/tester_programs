e13f4af9afee07b397aade7597c876609b3ec9bb
Stephen Bell <stephen.bell@setec.com.au>
2019-01-07 09:31:33 +1100

    Update GEN9 firmware.

--- programs/gen9/config.py
+++ programs/gen9/config.py
@@ -4,7 +4,7 @@
 """GEN9-540 Configuration."""
 
 SW_VER = '1.0'
-SW_SVN = '18371'
-SW_BUILD = '2505'
+SW_SVN = '18392'
+SW_BUILD = '2512'
 # Software image filename
 SW_IMAGE = 'gen9_{0}.{1}.{2}.bin'.format(SW_VER, SW_SVN, SW_BUILD)
