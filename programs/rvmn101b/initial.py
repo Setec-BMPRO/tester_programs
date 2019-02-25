d8d67730bbe38f32124b9ffe9eacdf528e082cd1
Stephen Bell <stephen.bell@setec.com.au>
2019-02-25 15:31:40 +1100

    Setup RVMN101B initial test.

--- programs/rvmn101b/initial.py
+++ programs/rvmn101b/initial.py
@@ -17,19 +17,22 @@ class Initial(share.TestSequence):
     """RVMN101B Initial Test Program."""
 
     limitdata = (
-        tester.LimitDelta('Vin', 3.3, 0.3),
+        tester.LimitDelta('Vbatt', 12.0, 0.5),
+        tester.LimitDelta('3V3', 3.3, 0.3),
+        tester.LimitBoolean('CANok', True, doc='CAN bus active'),
         tester.LimitBoolean('ScanMac', True, doc='MAC address detected'),
-        tester.LimitRegExp('BleMac', '^[0-9a-f]{12}$', doc='Valid MAC address'),
+        tester.LimitRegExp('BleMac', '^[0-9a-f]{12}$',
+            doc='Valid MAC address'),
         )
 
     def open(self, uut):
         """Create the test program as a linear sequence."""
-        Devices.sw_image = config.SW_IMAGE.format(self.parameter)
         super().open(self.limitdata, Devices, Sensors, Measurements)
         self.steps = (
             tester.TestStep('PowerUp', self._step_power_up),
             tester.TestStep('PgmARM', self.devices['progARM'].program),
             tester.TestStep('PgmNordic', self.devices['progNRF'].program),
+            tester.TestStep('CanBus', self._step_canbus),
             tester.TestStep('GetMac', self._step_get_mac),
             tester.TestStep('Bluetooth', self._step_bluetooth),
             )
@@ -37,29 +40,40 @@ class Initial(share.TestSequence):
 
     @share.teststep
     def _step_power_up(self, dev, mes):
-        """Apply input 3V3dc and measure voltages."""
+        """Apply input power and measure voltages."""
         self.sernum = self.get_serial(self.uuts, 'SerNum', 'ui_serialnum')
-        dev['dcs_vin'].output(3.3, output=True)
-        mes['dmm_vin'](timeout=5)
+        dev['dcs_vbatt'].output(12.0, output=True)
+        self.measure(('dmm_vbatt', 'dmm_3v3', ), timeout=5)
 
     @share.teststep
     def _step_get_mac(self, dev, mes):
         """Get the MAC address from the console."""
         # Open console serial connection
         dev['rvmn101b'].open()
-        dev['rla_nrf_reset'].disable(delay=0.1)
         # Cycle power to get the banner from the Nordic
         dev['dcs_vin'].output(0.0, delay=0.5)
         dev['rvmn101b'].port.flushInput()
-        dev['dcs_vin'].output(3.3, delay=0.1)
+        dev['dcs_vin'].output(12.0, delay=0.1)
         self.mac = dev['rvmn101b'].get_mac()
         mes['ble_mac'].sensor.store(self.mac)
         mes['ble_mac']()
 
     @share.teststep
+    def _step_canbus(self, dev, mes):
+        """Test the CAN Bus."""
+        candev = dev['can']
+        candev.flush_can()      # Flush all waiting packets
+        try:
+            candev.read_can()
+            result = True
+        except tester.devphysical.can.SerialToCanError:
+            result = False
+        mes['can_active'].sensor.store(result)
+        mes['can_active']()
+
+    @share.teststep
     def _step_bluetooth(self, dev, mes):
         """Test the Bluetooth interface."""
-        # Press Button2 to broadcast on bluetooth
         reply = dev['pi_bt'].scan_advert_blemac(self.mac, timeout=20)
         mes['scan_mac'].sensor.store(reply)
         mes['scan_mac']()
@@ -75,16 +89,12 @@ class Devices(share.Devices):
         for name, devtype, phydevname in (
                 ('dmm', tester.DMM, 'DMM'),
                 ('dcs_vcom', tester.DCSource, 'DCS1'),
-                ('dcs_vin', tester.DCSource, 'DCS2'),
+                ('dcs_vbatt', tester.DCSource, 'DCS2'),
+                ('dcs_vhbridge', tester.DCSource, 'DCS3'),
                 ('rla_reset', tester.Relay, 'RLA1'),
                 ('rla_boot', tester.Relay, 'RLA2'),
-                ('rla_nrf_reset', tester.Relay, 'RLA22'),
             ):
             self[name] = devtype(self.physical_devices[phydevname])
-        # Some more obvious ways to use this relay
-        rla = self['rla_nrf_reset']
-        rla.disable = rla.set_off
-        rla.enable = rla.set_on
         # Working folder
         folder = os.path.dirname(
             os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -106,18 +116,27 @@ class Devices(share.Devices):
         self['rvmn101b'] = console.Console(nordic_ser)
         # Connection to RaspberryPi bluetooth server
         self['pi_bt'] = share.bluetooth.RaspberryBluetooth()
+        # CAN interface
+        self['can'] = self.physical_devices['_CAN']
+        self['can'].rvc_mode = True
+        self['can'].verbose = True
+        self.add_closer(self.close_can)
         # Fixture USB hub power
         self['dcs_vcom'].output(9.0, output=True, delay=10)
         self.add_closer(lambda: self['dcs_vcom'].output(0.0, output=False))
 
     def reset(self):
         """Reset instruments."""
-        for dcs in ('dcs_vin', ):
+        for dcs in ('dcs_vbatt', ):
             self[dcs].output(0.0, False)
-        for rla in ('rla_pos1', 'rla_reset', 'rla_boot'):
+        for rla in ('rla_reset', 'rla_boot'):
             self[rla].set_off()
-        self['rla_nrf_reset'].disable()
         self['rvmn101b'].close()
+
+    def close_can(self):
+        """Restore CAN interface to default settings."""
+        self['can'].rvc_mode = False
+        self['can'].verbose = False
 
 
 class Sensors(share.Sensors):
@@ -128,9 +147,11 @@ class Sensors(share.Sensors):
         """Create all Sensors."""
         dmm = self.devices['dmm']
         sensor = tester.sensor
-        self['mirmac'] = sensor.Mirror(rdgtype=sensor.ReadingString)
-        self['mirscan'] = sensor.Mirror(rdgtype=sensor.ReadingBoolean)
-        self['vin'] = sensor.Vdc(dmm, high=1, low=1, rng=10, res=0.01)
+        self['MirMAC'] = sensor.Mirror(rdgtype=sensor.ReadingString)
+        self['MirScan'] = sensor.Mirror(rdgtype=sensor.ReadingBoolean)
+        self['MirCAN'] = sensor.Mirror(rdgtype=sensor.ReadingBoolean)
+        self['vbatt'] = sensor.Vdc(dmm, high=1, low=1, rng=100, res=0.01)
+        self['3v3'] = sensor.Vdc(dmm, high=2, low=1, rng=10, res=0.01)
         self['SnEntry'] = sensor.DataEntry(
             message=tester.translate('rvmn101b_initial', 'msgSnEntry'),
             caption=tester.translate('rvmn101b_initial', 'capSnEntry'))
@@ -150,9 +171,11 @@ class Measurements(share.Measurements):
     def open(self):
         """Create all Measurements."""
         self.create_from_names((
-            ('dmm_vin', 'Vin', 'vin', ''),
+            ('dmm_3v3', '3V3', '3v3', ''),
+            ('dmm_vbatt', 'Vbatt', 'vbatt', ''),
             ('ui_serialnum', 'SerNum', 'SnEntry', ''),
-            ('ble_mac', 'BleMac', 'mirmac', 'Get MAC address from console'),
-            ('scan_mac', 'ScanMac', 'mirscan',
+            ('can_active', 'CANok', 'MirCAN', 'CAN bus traffic seen'),
+            ('ble_mac', 'BleMac', 'MirMAC', 'Get MAC address from console'),
+            ('scan_mac', 'ScanMac', 'MirScan',
                 'Scan for MAC address over bluetooth'),
             ))
