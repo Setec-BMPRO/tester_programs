795b93691d2acf7eaa25801ccfeff7de1e079923
Stephen Bell <stephen.bell@setec.com.au>
2019-07-31 12:41:50 +1000

    Remove banner_lines patch.
    It works fine without the patch.

--- programs/rvswt101/console.py
+++ programs/rvswt101/console.py
@@ -22,12 +22,9 @@ class Console(share.console.Base):
 
         """
         mac = self.action(None, delay=1.5, expected=self.banner_lines)
-# FIXME: Why is banner_lines != 2 for the 'j' types?
-#        if self.banner_lines > 1:
-#            mac = mac[0]
-#        mac = mac.replace(':', '')
-        mac = mac[0].replace(':', '')
-
+        if self.banner_lines > 1:
+            mac = mac[0]
+        mac = mac.replace(':', '')
         match = self.re_banner.match(mac)
         if not match:
             raise ValueError('Bluetooth MAC not found in startup banner')
