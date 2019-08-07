48636e9c96b641813370ebdd0cc4bc5830d86f46
Stephen Bell <stephen.bell@setec.com.au>
2019-08-07 16:53:08 +1000

    Update SX-600 firmware.

--- programs/sx600_750/config.py
+++ programs/sx600_750/config.py
@@ -90,7 +90,7 @@ class SX600(Config):
 
     # Initial Test parameters
     #  Software version
-    _bin_version = '1.0.19260.1260'
+    _bin_version = '1.0.19270.1261'
     #  Software image filenames
     arm_bin = 'sx600_{0}.bin'.format(_bin_version)
     # PFC digital pot sensitivity (V/step)
