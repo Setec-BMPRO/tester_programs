8b00b6cdd72e2aa678658e8be359c2249349456e
Stephen Bell <stephen.bell@setec.com.au>
2020-08-04 09:32:26 +1000

    Implement ECO-23077 for RVMN101B.
    Updated firmware.

--- programs/rvmn101/config.py
+++ programs/rvmn101/config.py
@@ -209,6 +209,7 @@ class RVMN101B(Config):
     _nordic_088 = 'tmc_rvmn101_signed_0.88-0-g5f64a82_factory_mcuboot.hex'
     _nordic_089 = 'tmc_rvmn101_signed_0.89-0-g9fc6374f_factory_mcuboot.hex'
     _nordic_092 = 'tmc_rvmn101_signed_0.92-0-g015c3552_factory_mcuboot.hex'
+    _nordic_093 = 'tmc_rvmn101_signed_0.93-0-g58b51527_factory_mcuboot.hex'
     _arm_image_19 = 'rvmn101_nxp_1.9.bin'
     # Lot number mapping
     _lot_rev = share.lots.Revision((
@@ -216,12 +217,13 @@ class RVMN101B(Config):
         (share.lots.Range('A200820', 'A202704'), 6),    # 034229
         (share.lots.Range('A202722', 'A202809'), 7),    # 034940
         (share.lots.Range('A202907', 'A203113'), 8),    # 034940 PC-22885
-        # Rev 8...                                      # 035120
+        (share.lots.Range('A203303', 'A203303'), 8),    # 035120
+        # Rev 9...                                      # 035231
         ))
     _rev_data = {
         None: _Values(
-            nordic_image=_nordic_092, arm_image=_arm_image_19,
-            product_rev='08A', hardware_rev=None, banner_lines=4,
+            nordic_image=_nordic_093, arm_image=_arm_image_19,
+            product_rev='09A', hardware_rev=None, banner_lines=4,
             reversed_output_dict={},
             ),
         8: _Values(
