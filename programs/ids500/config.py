282f138c24562e08e72475cf27af82b616e6231d
Stephen Bell <stephen.bell@setec.com.au>
2020-10-19 11:12:40 +1100

    Relax IDS500 LDD limits. Pre Rev 9 unit cannot meet spec.

--- programs/ids500/config.py
+++ programs/ids500/config.py
@@ -13,7 +13,14 @@ pic_hex_syn = 'ids_picSyn_2.hex'
 #       IS_Set (0-5V) to Iout (0-50A):     ±(0.7% + 10mA)
 #       Iout (0-50A) to IS_Iout (0-5V):    ±(0.7% +  5mV)  [ 5mV == 50mA ]
 #       IS_Set (0-5V) to IS_Iout (0-5V):   <Not specified>
-ldd_set_out_error_6 = 0.052
+#
+# FIXME: Limits disabled until Rev 9 is released
+#   Rev <9 cannot meet the specifications...
+#ldd_set_out_error_6 = 0.052
+#ldd_out_mon_error_6 = 0.092
+#ldd_set_out_error_50 = 0.36
+#ldd_out_mon_error_50 = 0.40
+ldd_set_out_error_6 = 0.07
 ldd_out_mon_error_6 = 0.092
-ldd_set_out_error_50 = 0.36
-ldd_out_mon_error_50 = 0.40
+ldd_set_out_error_50 = 0.7
+ldd_out_mon_error_50 = 0.7
