43de5ad0daa35a5d5fc9030ff97f9e4fba554c5a
Stephen Bell <stephen.bell@setec.com.au>
2021-01-20 13:50:32 +1100

    Implement PC-24906 for TRS-BTS.

--- programs/trsbts/config.py
+++ programs/trsbts/config.py
@@ -17,6 +17,6 @@ class TrsBts():
 
     """TRS-BTS configuration."""
 
-    sw_version = '1.0.19839.2135'
+    sw_version = '2.0.20494.2194'       # PC-24906
     sw_image = 'trs-bts_factory_{0}.hex'.format(sw_version)
-    hw_version = (3, 0, 'A')
+    hw_version = (3, 0, 'B')
