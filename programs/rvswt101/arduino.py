f6cfdb4baf7289ebb3d696c901c3c6eac40abe1f
Chris Conlan <chris.conlan@setec.com.au>
2021-07-02 16:37:26 +1000

    Changes to the RVSWT101 arduino console
    Use default console write_format, specify the console read_format.

--- programs/rvswt101/arduino.py
+++ programs/rvswt101/arduino.py
@@ -10,26 +10,27 @@ class Arduino(share.console.Base):
     """Communications to RVSWT101 Arduino console."""
     cmd_data = {
         'DEBUG': share.console.parameter.String(
-            'DEBUG_ON', writeable=True, write_format='{1}'),
+            'DEBUG_ON', read_format='{0}'),
         'QUIET': share.console.parameter.String(
-            'DEBUG_OFF', writeable=True, write_format='{1}'),
+            'DEBUG_OFF', read_format='{0}'),
         # Actuator commands
         'RETRACT_ACTUATORS': share.console.parameter.String(
-            'ACTU_NONE', writeable=True, write_format='{1}'),
+            'ACTU_NONE', read_format='{0}'),
         'EJECT_DUT': share.console.parameter.String(
-            'ACTU_EJECT', writeable=True, write_format='{1}'),
+            'ACTU_EJECT', read_format='{0}'),
         # 4 or 6 button tester configuration
         '4BUTTON_MODEL': share.console.parameter.String(
-            '4BUTTON', writeable=True, write_format='{1}'),
+            '4BUTTON', read_format='{0}'),
         '6BUTTON_MODEL': share.console.parameter.String(
-            '6BUTTON', writeable=True, write_format='{1}'),
+            '6BUTTON', read_format='{0}'),
         }
+
     # Build all 6 PRESS_BUTTON and RELEASE_BUTTON actuator commands and add to cmd_data
     for n in range(1, 7):
         commands = (('PRESS_BUTTON_{}'.format(n), 'ACTU{};1'.format(n)),
                     ('RELEASE_BUTTON_{}'.format(n), 'ACTU{};0'.format(n)))
         for cmds in commands:
-            cmd_data[cmds[0]] = share.console.parameter.String(cmds[1], writeable=True, write_format='{1}')
+            cmd_data[cmds[0]] = share.console.parameter.String(cmds[1], writeable=True, write_format='{1}', read_format='{0}')
 
     def init_command(self, command):
         """init the console command."""
