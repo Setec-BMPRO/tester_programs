bdcaed767663f50e26f1064d21a97297690dcfb7
Chris Conlan <chris.conlan@setec.com.au>
2021-08-03 11:09:00 +1000

    RVSWT101 Final tidy up and get testcase working.
    Change testcase from 4 to 6 button model.
    Remove unused sensor measurements from RVSWT101 Final testcase.
    Modify arduino.py to satisfy testcase (writeable=True and write_format).

--- programs/rvswt101/arduino.py
+++ programs/rvswt101/arduino.py
@@ -9,24 +9,25 @@ class Arduino(share.console.Base):
 
     """Communications to RVSWT101 Arduino console."""
     cmd_data = {
-        'DEBUG': share.console.parameter.String(
-            'DEBUG_ON', read_format='{0}'),
-        'QUIET': share.console.parameter.String(
-            'DEBUG_OFF', read_format='{0}'),
+        'DEBUG': share.console.parameter.String('DEBUG_ON',
+            writeable=True, write_format='{1}', read_format='{0}'),
+        'QUIET': share.console.parameter.String('DEBUG_OFF',
+            writeable=True, write_format='{1}', read_format='{0}'),
         # Actuator commands
-        'RETRACT_ACTUATORS': share.console.parameter.String(
-            'ACTU_NONE', read_format='{0}'),
-        'EJECT_DUT': share.console.parameter.String(
-            'ACTU_EJECT', read_format='{0}'),
+        'RETRACT_ACTUATORS': share.console.parameter.String('ACTU_NONE',
+            writeable=True, write_format='{1}', read_format='{0}'),
+        'EJECT_DUT': share.console.parameter.String('ACTU_EJECT',
+            writeable=True, write_format='{1}', read_format='{0}'),
+        'EXERCISE': share.console.parameter.String('EXERCISE',
+            writeable=True, write_format='{1}', read_format='{0}'),
         # 4 or 6 button tester configuration
-        '4BUTTON_MODEL': share.console.parameter.String(
-            '4BUTTON', read_format='{0}'),
-        '6BUTTON_MODEL': share.console.parameter.String(
-            '6BUTTON', read_format='{0}'),
-        'EXERCISE': share.console.parameter.String(
-            'EXERCISE', read_format='{0}'),
-        'UUT': share.console.parameter.String(
-            'UUT', read_format='{0}'),
+        '4BUTTON_MODEL': share.console.parameter.String('4BUTTON',
+            writeable=True, write_format='{1}', read_format='{0}'),
+        '6BUTTON_MODEL': share.console.parameter.String('6BUTTON',
+            writeable=True, write_format='{1}', read_format='{0}'),
+        # UUT detection
+        'UUT': share.console.parameter.String('UUT',
+            writeable=True, write_format='{1}', read_format='{0}'),
         }
 
     # Build all 6 PRESS_BUTTON and RELEASE_BUTTON actuator commands and add to cmd_data
@@ -34,8 +35,8 @@ class Arduino(share.console.Base):
         commands = (('PRESS_BUTTON_{}'.format(n), 'ACTU{};1'.format(n)),
                     ('RELEASE_BUTTON_{}'.format(n), 'ACTU{};0'.format(n)))
         for test_cmd, arduino_cmd in commands:
-            cmd_data[test_cmd] = share.console.parameter.String(arduino_cmd,  writeable=True,
-                                 write_format='{1}', read_format='{0}')
+            cmd_data[test_cmd] = share.console.parameter.String(arduino_cmd,
+                writeable=True, write_format='{1}', read_format='{0}')
 
     def init_command(self, command):
         """init the console command."""
