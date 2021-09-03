58f158cecaeeed996023ffa9b629dbb0c53ee760
Stephen Bell <stephen.bell@setec.com.au>
2021-09-03 16:30:38 +1000

    Implement IDS-500 Rev 9 test limits.

--- programs/ids500/config.py
+++ programs/ids500/config.py
@@ -14,13 +14,14 @@ pic_hex_syn = 'ids_picSyn_2.hex'
 #       Iout (0-50A) to IS_Iout (0-5V):    ±(0.7% +  5mV)  [ 5mV == 50mA ]
 #       IS_Set (0-5V) to IS_Iout (0-5V):   <Not specified>
 #
-# FIXME: Limits disabled until Rev 9 is released
+# Rev 9+ limits
 #   Rev <9 cannot meet the specifications...
-#ldd_set_out_error_6 = 0.052
-#ldd_out_mon_error_6 = 0.092
-#ldd_set_out_error_50 = 0.36
-#ldd_out_mon_error_50 = 0.40
-ldd_set_out_error_6 = 0.07
+ldd_set_out_error_6 = 0.052
 ldd_out_mon_error_6 = 0.092
-ldd_set_out_error_50 = 0.7
-ldd_out_mon_error_50 = 0.7
+ldd_set_out_error_50 = 0.36
+ldd_out_mon_error_50 = 0.40
+# Pre-Rev 9 limits
+#ldd_set_out_error_6 = 0.07
+#ldd_out_mon_error_6 = 0.092
+#ldd_set_out_error_50 = 0.7
+#ldd_out_mon_error_50 = 0.7
