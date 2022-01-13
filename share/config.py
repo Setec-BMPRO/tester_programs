64ee81ae0b8eeef5e23e76491e087f483d029f85
Stephen Bell <stephen.bell@setec.com.au>
2022-01-13 16:14:11 +1100

    Use CANable interface on all testers.

--- share/config.py
+++ share/config.py
@@ -35,40 +35,40 @@ class Fixture():
     """Fixture specific data, such as serial port assignment.
 
         Linux testers:
-           Use /dev/ttyACM0 for the GEN4 (Arduino) device.
-           /dev/ttyUSB0 is used for the Serial2Can interface device.
+           /dev/ttyACM0 is the CANable Pro interface device.
+           /dev/ttyACM1 is the ATE4 GEN4 (Arduino) device.
 
         Windows testers:
            COM port mapping must be manually set on each tester.
-           COM30 is used for the Serial2Can interface device.
+           COM30 is used for the CANable Pro interface device.
 
     """
 
     # A single direct connected FTDI, ID: 0403:6001, without S/N
-    _ftdi = {'posix': '/dev/ttyUSB1', 'nt': 'COM16'}[os.name]
+    _ftdi = {'posix': '/dev/ttyUSB0', 'nt': 'COM16'}[os.name]
     # FTDI without S/N connected via a USB Hub
-    _ftdi_hub_1 = {'posix': '/dev/ttyUSB1', 'nt': 'COM14'}[os.name]
-    _ftdi_hub_2 = {'posix': '/dev/ttyUSB2', 'nt': 'COM15'}[os.name]
+    _ftdi_hub_1 = {'posix': '/dev/ttyUSB0', 'nt': 'COM14'}[os.name]
+    _ftdi_hub_2 = {'posix': '/dev/ttyUSB1', 'nt': 'COM15'}[os.name]
 
     _data = {
 
         # Fixtures with a single USB Serial (inc. FTDI with S/N)
 
         '017048': {     # IDS-500 Final (Prolific)
-            'PIC': {'posix': '/dev/ttyUSB1', 'nt': 'COM6'}[os.name],
+            'PIC': {'posix': '/dev/ttyUSB0', 'nt': 'COM6'}[os.name],
             },
         '017054': {     # IDS-500 Main Initial (Prolific) [ Unused ]
-            'PIC': {'posix': '/dev/ttyUSB1', 'nt': 'COM6'}[os.name],
+            'PIC': {'posix': '/dev/ttyUSB0', 'nt': 'COM6'}[os.name],
             },
         '017823': {     # C45A-15 Initial
-            'ARDUINO': {'posix': '/dev/ttyACM0', 'nt': 'COM37'}[os.name],
+            'ARDUINO': {'posix': '/dev/ttyACM1', 'nt': 'COM37'}[os.name],
             },
         '019883': {     # ETrac-II Initial
-            'ARDUINO': {'posix': '/dev/ttyACM0', 'nt': 'COM36'}[os.name],
+            'ARDUINO': {'posix': '/dev/ttyACM1', 'nt': 'COM36'}[os.name],
             },
         '027013': {     # BatteryCheck Final
             # Panasonic eUniStone PAN1322 (FTDI with S/N)
-            'BT': {'posix': '/dev/ttyUSB1', 'nt': 'COM9'}[os.name],
+            'BT': {'posix': '/dev/ttyUSB0', 'nt': 'COM9'}[os.name],
             },
 
         # Fixtures with a single FTDI without any S/N
@@ -91,9 +91,9 @@ class Fixture():
 
         '017789': {     # CMR-SBP Initial (Prolific)
             # Hub port 1:
-            'EV': {'posix': '/dev/ttyUSB1', 'nt': 'COM21'}[os.name],
+            'EV': {'posix': '/dev/ttyUSB0', 'nt': 'COM21'}[os.name],
             # Hub port 2:
-            'CMR': {'posix': '/dev/ttyUSB2', 'nt': 'COM22'}[os.name],
+            'CMR': {'posix': '/dev/ttyUSB1', 'nt': 'COM22'}[os.name],
             },
         '020827': {     # BCE282 Initial
             'MSP1': _ftdi_hub_1,    # Programming
@@ -102,12 +102,12 @@ class Fixture():
         '022837': {     # SX-600/SX-750 Initial
             'ARM': _ftdi_hub_1,
             # Hub port 2:
-            'ARDUINO': {'posix': '/dev/ttyACM0', 'nt': 'COM5'}[os.name],
+            'ARDUINO': {'posix': '/dev/ttyACM1', 'nt': 'COM5'}[os.name],
             },
         '027176': {     # BP35 Initial
             'ARM': _ftdi_hub_1,
             # Hub port 2:
-            'ARDUINO': {'posix': '/dev/ttyACM0', 'nt': 'COM39'}[os.name],
+            'ARDUINO': {'posix': '/dev/ttyACM1', 'nt': 'COM39'}[os.name],
             },
         '028468': {     # CN101,2,3 Initial
             'ARM': _ftdi_hub_1,
@@ -144,7 +144,7 @@ class Fixture():
         '034400': {     # BP35-II Initial
             'ARM': _ftdi_hub_1,
             # Hub port 2:
-            'ARDUINO': {'posix': '/dev/ttyACM0', 'nt': 'COM38'}[os.name],
+            'ARDUINO': {'posix': '/dev/ttyACM1', 'nt': 'COM38'}[os.name],
             },
         '034861': {     # RVMN5x Initial
             'ARM': _ftdi_hub_1,
@@ -175,9 +175,9 @@ class Fixture():
 
         """
         result = cls._data[fixture][name]
-        # ATE4 has the GEN4 Arduino as ttyACM0
-        if System.tester_type == 'ATE4' and result == '/dev/ttyACM0':
-            result = '/dev/ttyACM1'
+        # ATE4 has the GEN4 Arduino as ttyACM1
+        if System.tester_type == 'ATE4' and result == '/dev/ttyACM1':
+            result = '/dev/ttyACM2'
         return result
 
     @classmethod
