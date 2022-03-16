5aff907dac3b82220213f39b77006acf583612d1
Stephen Bell <stephen.bell@setec.com.au>
2022-03-16 20:40:02 +1100

    Use a worker thread to program ARM devices with isplpc.

--- share/programmer.py
+++ share/programmer.py
@@ -7,6 +7,7 @@ import abc
 import pathlib
 import os
 import subprocess
+import threading
 import time
 
 import isplpc
@@ -129,50 +130,68 @@ class ARM(_Base):
         self.baudrate = 115200
         self.crpmode = crpmode
         self._bindata = None
+        self._ser = None
+        self._worker = None
 
     def program_begin(self):
-        """Program a device.
+        """Begin programming a device.
 
         If BOOT or RESET relay devices are available, use them to put the chip
         into bootloader mode (Assert BOOT, pulse RESET).
         BDA4 serial signals: RTS = BOOT, DTR = RESET.
 
         """
+        # Read the image & open serial port on main thread
         if not self._bindata:
             with self.file.open('rb') as infile:
                 self._bindata = bytearray(infile.read())
-        ser = serial.Serial(port=self.port, baudrate=self.baudrate)
+        self._ser = serial.Serial(port=self.port, baudrate=self.baudrate)
         # We need to wait just a little before flushing the port
         time.sleep(0.5)
-        ser.reset_input_buffer()
-        try:
-            if self.bda4_signals:
-                ser.rts = ser.dtr = True
-                ser.dtr = False
+        self._ser.reset_input_buffer()
+        # Device I/O activity is only done on main thread
+        if self.bda4_signals:
+            self._ser.rts = self._ser.dtr = True    # Assert BOOT & RESET
+            time.sleep(0.01)
+            self._ser.dtr = False                   # Release RESET
+            time.sleep(0.01)
+            self._ser.rts = False                   # Release BOOT
+        else:
             if self.boot_relay:
-                self.boot_relay.set_on()
+                self.boot_relay.set_on()            # Assert BOOT
             if self.reset_relay:
-                self.reset_relay.pulse(0.1)
-            pgm = isplpc.Programmer(
-                ser,
-                self._bindata,
-                erase_only=False,
-                verify=False,
-                crpmode=self.crpmode)
-            try:
-                pgm.program()
-                self.result = self.pass_result
-            except isplpc.ProgrammingError as exc:
-                self.result = str(exc)
-        finally:
-            if self.bda4_signals:
-                ser.rts = False
-            ser.close()
+                self.reset_relay.pulse(0.1)         # Pulse RESET
             if self.boot_relay:
-                self.boot_relay.set_off()
+                self.boot_relay.set_off()           # Release BOOT
+        # Target device is now running in ISP mode
+        pgm = isplpc.Programmer(
+            self._ser,
+            self._bindata,
+            erase_only=False,
+            verify=False,
+            crpmode=self.crpmode)
+        self._worker = threading.Thread(
+            target=self.worker, name='ARMthread', args=(pgm, ))
+        self._worker.start()
+
+    def worker(self, pgm):
+        """Worker thread to do the programming.
+
+        @param pgm islpc.Programmer instance
+
+        """
+        result = self.pass_result
+        try:
+            pgm.program()
+        except isplpc.ProgrammingError as exc:
+            result = str(exc)
+        self.result = result
 
     def program_wait(self):
         """Wait for device programming to finish."""
+        self._worker.join()
+        self._ser.close()
+        self._ser = None
         self.result_check()
 
 
