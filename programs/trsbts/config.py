a8ac306f653674def238394f5b4615976e348f2d
Stephen Bell <stephen.bell@setec.com.au>
2022-12-21 10:06:33 +1100

    TRS-BTx Rev 4 Blue LED is no so bright.

--- programs/trsbts/config.py
+++ programs/trsbts/config.py
@@ -8,15 +8,15 @@ class TrsBt2:
 
     """TRS-BT2 configuration."""
 
-    sw_version = "2.0.20494.2194"  # PC-24735
+    sw_version = "2.0.20494.2194"
     sw_image = "trs-bts_factory_{0}.hex".format(sw_version)
-    hw_version = (3, 0, "B")
+    hw_version = (4, 0, "A")
 
 
 class TrsBts:
 
     """TRS-BTS configuration."""
 
-    sw_version = "2.0.20494.2194"  # PC-24906
+    sw_version = "2.0.20494.2194"
     sw_image = "trs-bts_factory_{0}.hex".format(sw_version)
-    hw_version = (3, 0, "B")
+    hw_version = (5, 0, "A")
