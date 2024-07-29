43ef2ce159e72de1d73e242195bd253e9485c486
Stephen Bell <stephen.bell@setec.com.au>
2024-07-29 14:13:12 +1000

    Update BC60 STM firmware for pilot.

--- programs/bc60/initial.py
+++ programs/bc60/initial.py
@@ -32,7 +32,7 @@ class Initial(share.TestSequence):
     def open(self):
         """Prepare for testing."""
         Sensors.ble_image = "bl652_hci_uart_v1.0.0-0-ga43a850.hex"
-        Sensors.stm_image = "bc60_v1.0.1-0-g25dfa71-signed-mcuboot-factory.hex"
+        Sensors.stm_image = "bc60_v1.0.2-0-g65bd136-signed-mcuboot-factory.hex"
         self.configure(self.initial_limits, Devices, Sensors, Measurements)
         super().open()
         self.steps = (
